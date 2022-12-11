2bc5b68abdd445e8d52fb54879e8653dea0d09d3
Dennis Whalen <dwwhalen@gmail.com>
2022-12-11 11:07:02 -0500

    expanding the calculator example

--- robot-framework/e2e/libraries/MyCalculatorApplication.py
+++ robot-framework/e2e/libraries/MyCalculatorApplication.py
@@ -1,10 +1,9 @@
 def start_calculator():
     print("Calculator Started")
 
-def calculate_term(term):
+def do_math(term):
     """
     Interpret term as a mathematical expression
     and return the result
     """
-    print("Calculating Term:", term)
     return eval(term)
